0c2b4b23833530822b2c42be6e360ee5af14bd96
wcode_jannikola <nikola.jankovic@waltercode.com>
2020-02-17 12:03:53 +0100

    sharing location & event acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,31 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message
-    socket.emit('sendMessage', message.value)
+    socket.emit('sendMessage', message.value, (error) => {
+        if(error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
     message.value = ''
+})
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+    if(!navigator.geolocation) {
+        return alert('geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
+        })
+    })
+})
+
+socket.on('sendLocation', (position) => {
+    console.log(`this is user location: ${position}`)
 })
