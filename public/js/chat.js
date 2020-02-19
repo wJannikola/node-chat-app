cd261a40b13e4927cdf79418ebde5f61b5af11a4
wcode_jannikola <nikola.jankovic@waltercode.com>
2020-02-19 16:26:55 +0100

    commented code removed

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,21 +13,16 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 const autoscroll = () => {
-    // New message element
     const $newMessage = $messages.lastElementChild
 
-    // Height of the new message
     const newMessageStyles = getComputedStyle($newMessage)
     const newMessageMargin = parseInt(newMessageStyles.marginBottom)
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
 
-    // Visible height
     const visibleHeight = $messages.offsetHeight
 
-    // Height of messages container
     const containerHeight = $messages.scrollHeight
 
-    // How far have I scrolled?
     const scrollOffset = $messages.scrollTop + visibleHeight
 
     if (containerHeight - newMessageHeight <= scrollOffset) {
